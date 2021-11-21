c897be7c5032bb1bfd0cca2f1675702cbdbb2222
JanmeetIITrpr <92101890+JanmeetIITrpr@users.noreply.github.com>
2021-11-21 16:48:18 +0530

    Update and rename treapnode.c to treapnodestruct.c

---
+++ src/treapnodestruct.c
@@ -0,0 +1,7 @@
+struct Node{                            //struct Node which
+    int key;                            //stores the key value of the node
+    int pr;                             //stores the priority of each node
+    struct Node* left;                  //pointer to the left child
+    struct Node* right;                 //pointer to the right child
+};
+
