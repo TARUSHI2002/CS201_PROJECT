e3846765d568f7adaba1ec0e2efbe1d503c046c8
JanmeetIITrpr <92101890+JanmeetIITrpr@users.noreply.github.com>
2021-11-21 15:13:41 +0530

    Create delete.c

---
+++ delete.c
@@ -0,0 +1,50 @@
+struct Node* delete_Node(struct Node* root,int val)    //function to delete a node
+{
+    if(root==NULL)                                     //if treap is empty
+    return root;
+
+    if(val < root->key)                               //if key is in left sub-tree
+    root->left = delete_Node(root->left,val);
+
+    else if(val > root->key)                          //if key is in right sub-tree
+    root->right = delete_Node(root->right, val);
+
+
+    //If key is at Root
+
+    //If the left child of the key is NULL
+    else if(root->left == NULL)
+    {
+        //Make right child as root node
+        struct Node* temp = root->right;
+        //root=NULL;
+        free(root);
+        root=temp;
+        //return root;
+    }
+
+    //If the right child of the key is NULL
+    else if(root->right == NULL)
+    {
+        //Make left child as root node 
+        struct Node* temp = root->left;
+        //root=NULL;
+        free(root);
+        root=temp;
+        //return root;
+    }
+
+    //If both left and right child of the root(key) are not NULL
+    //maintaining the heap property and deleting the node
+    else if(root->left->pr < root->right->pr)               
+    {
+        root = leftRotate(root);                      
+        root->left = delete_Node(root->left,val);
+    }
+    else
+    {
+        root = rightRotate(root);
+        root->right = delete_Node(root->right, val);
+    }
+    return root;        //returning the root
+}
